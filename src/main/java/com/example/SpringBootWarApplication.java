0fa25cacddfa31fffd23ac47d4fcd88e0d32ec05
rav <rachana.kaparthi@iiitb.ac.in>
2025-04-29 14:44:15 +0530

    first java project setup

---
+++ src/main/java/com/example/SpringBootWarApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootWarApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWarApplication.class, args);
+    }
+}
